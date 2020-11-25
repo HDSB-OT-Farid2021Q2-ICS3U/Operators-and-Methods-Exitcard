173b9af1138c3847b421f0e54a01095d7aca42b2
Farid <71142299+Mr-Farid@users.noreply.github.com>
2020-11-25 09:01:16 -0500

    Create main.py

---
+++ main.py
@@ -0,0 +1,4 @@
+# Take the input of a name for a triangle along with its base and height measurement
+# Ouput the name of the triangle without vowels (including 'y') along with the area of the triangle
+# Do not use anything we haven't learned in class yet (no if, loops, arrays)
+
